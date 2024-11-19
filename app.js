b924ba348ad2698b4bc44aa615202e42a3757538
Solstice <leon.t.isip@gmail.com>
2024-11-19 15:16:31 +0800

    added broken js functionality, the number-input getting changed and failure to display the proper output

--- app.js
+++ app.js
@@ -1,1 +1,35 @@
-console.log('Happy developing ✨')
+const userInput = document.getElementById('number-input');
+const convertButton = document.getElementById('convert-btn');
+const resultDiv = document.getElementById('result');
+
+
+convertButton.addEventListener('click', convertToRoman);
+
+
+function convertToRoman(num) {
+    const romanNumerals = [{value: 1000, symbol: "M"}, {value: 900, symbol: "CM"}, {
+        value: 500, symbol: "D"
+    }, {value: 400, symbol: "CD"}, {value: 100, symbol: "C"}, {value: 90, symbol: "XC"}, {
+        value: 50, symbol: "L"
+    }, {value: 40, symbol: "XL"}, {value: 10, symbol: "X"}, {value: 9, symbol: "IX"}, {
+        value: 5, symbol: "V"
+    }, {value: 4, symbol: "IV"}, {value: 1, symbol: "I"},]
+
+
+    let result = [];
+
+    if(userInput.value === '' || userInput.value < 0 || userInput.value > 3999) {
+        alert('Please enter a valid number!');
+    }
+
+    for (let i = 0; i < romanNumerals.length; i++) {
+        while (userInput.value >= romanNumerals[i].value) {
+        }
+        result[i] += romanNumerals[i].symbol;
+        userInput.value -= romanNumerals[i].value;
+    }
+    result.join('');
+    resultDiv.replaceChildren(result);
+}
+
+
